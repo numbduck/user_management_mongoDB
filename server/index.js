60bb07c92870633e8b62eb2f7dfae5f91ca66997
numbduck <109739553+numbduck@users.noreply.github.com>
2023-07-27 23:37:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,22 +7,22 @@ const mongoose = require("mongoose")
 app.use(cors());
 app.use(express.json());
 
-app.use(cors(
-  {
-    origin : ["https://user-management-mongo-db.vercel.app/"],
-    methods :["POST", "GET", "DELETE", "PUT"],
-    credentials : true
-  }
-))
+// app.use(cors(
+//   {
+//     origin : ["https://user-management-mongo-db.vercel.app/"],
+//     methods :["POST", "GET", "DELETE", "PUT"],
+//     credentials : true
+//   }
+// ))
 
 mongoose.connect("mongodb+srv://adityagreen:3zIYi9sYmrS5IWG1@cluster0.ynhw5pu.mongodb.net/users_db",{useNewUrlParser: true, useUnifiedTopology:true, dbName:'users_db'});
 
 
 const userSchema = new mongoose.Schema({
-  name:String,
-  email:String,
-  id:Number,
-  phone:Number
+  name: String,
+  email: String,
+  id: Number,
+  phone: Number
 })
 
 const UserModel = mongoose.model("users", userSchema);
